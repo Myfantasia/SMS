8063834c2523519dcb92379010dea8ca79a2e06a
MyFantasia <jordanmakesusmile@gmail.com>
2026-02-18 12:59:47 +0300

    student auth

--- schoolmanagement/urls.py
+++ schoolmanagement/urls.py
@@ -53,11 +53,13 @@ path('api/firebase-admin-signup/', views.firebase_admin_signup_bridge, name='fir
     path('admin-student', views.admin_student_view,name='admin-student'),
     path('admin-add-student', views.admin_add_student_view,name='admin-add-student'),
     path('admin-view-student', views.admin_view_student_view,name='admin-view-student'),
+
     path('delete-student-from-school/<int:pk>', views.delete_student_from_school_view,name='delete-student-from-school'),
     path('delete-student/<int:pk>', views.delete_student_view,name='delete-student'),
     path('update-student/<int:pk>', views.update_student_view,name='update-student'),
     path('admin-approve-student', views.admin_approve_student_view,name='admin-approve-student'),
     path('approve-student/<int:pk>', views.approve_student_view,name='approve-student'),
+    path('reject-student/<int:pk>', views.reject_student_view,name='reject-student'),
     path('admin-view-student-fee', views.admin_view_student_fee_view,name='admin-view-student-fee'),
 
 
